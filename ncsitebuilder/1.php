5d7a57e03a44e7c329b5c31b08e603ed7cd70e71
hoorayseo <126150637+hoorayseo@users.noreply.github.com>
2023-02-23 17:19:32 +0800

    Add files via upload

---
+++ ncsitebuilder/1.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title><?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : "懶人減肥方法"); ?></title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=1200" />
+		<meta name="description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta name="keywords" content="<?php echo htmlspecialchars(($seoKeywords !== "") ? $seoKeywords : ""); ?>" />
+		<!-- Facebook Open Graph -->
+	<meta property="og:title" content="<?php echo htmlspecialchars(($seoTitle !== "") ? $seoTitle : "懶人減肥方法"); ?>" />
+	<meta property="og:description" content="<?php echo htmlspecialchars(($seoDescription !== "") ? $seoDescription : ""); ?>" />
+	<meta property="og:image" content="<?php echo htmlspecialchars(($seoImage !== "") ? "{{base_url}}".$seoImage : ""); ?>" />
+	<meta property="og:type" content="article" />
+	<meta property="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-3.5.1.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20210903132712" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20210909181042" rel="stylesheet" type="text/css" id="wb-site-stylesheet" />
+	<link href="css/common.css?ts=1631337009" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1631337009" rel="stylesheet" type="text/css" id="wb-page-stylesheet" />
+	<ga-code/>
+	<script type="text/javascript">
+	window.useTrailingSlashes = true;
+	window.disableRightClick = true;
+</script>
+	
+	<link href="css/flag-icon-css/css/flag-icon.min.css" rel="stylesheet" type="text/css" />	
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<![endif]-->
+
+	</head>
+
+
+<body class="site <?php if (isset($wbPopupMode) && $wbPopupMode) echo ' popup-mode'; ?> " <?php if (isset($wbLandingPage) && $wbLandingPage) echo ' data-landing-page="'.$wbLandingPage.'"'; ?>><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance7" class="wb_element wb-menu" data-plugin="Menu"><ul class="hmenu" dir="ltr"><li><a href="" target="_self">健身室</a></li><li class="active"><a href="%E6%87%B6%E4%BA%BA%E6%B8%9B%E8%82%A5%E6%96%B9%E6%B3%95/" target="_self">懶人減肥方法</a></li><li><a href="%E9%87%8D%E9%87%8F%E8%A8%93%E7%B7%B4/" target="_self">重量訓練</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance8" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p style="text-align: center;"><span class="wb-stl-highlight"><a href="https://calfitness.com.hk/"><span style="color:rgba(230,54,0,1);"><strong>香港健身室</strong></span></a></span></p>
+</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance10" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><h2 class="wb-stl-heading2" style="text-align: center;"><span style="color:rgba(255,255,255,1);">懶人減肥方法</span></h2>
+</div><div id="wb_element_instance11" class="wb_element" data-plugin="CustomHtml" style=" overflow: hidden;"><div style="height: 100%;"><h2>
+懶人減肥運動方法</h2>
+許多人想通過到<a href="https://calfitness.com.hk/" target="_blank">健身室</a>鍛煉來減肥，但是由硬太懶了而無法堅持下去，有沒有什麼既能鍛煉又不那麼辛苦的懶人減肥運動呢？有的，今天為大家介紹下面的幾種通過鍛煉來減肥的方法。<br><img alt="減肥方法" border="0" src="https://calfitness.com.hk/gallery/lose%20weight.JPG" title="GYM"><br><h3>
+1、乘坐公共汽車上的時候不要急著坐位子</h3>
+<br>
+到學校之後馬上就要坐好幾個小時，到辦公室也一樣。為什麼急著坐公共汽車上的座位？更重要的是，如果是站著的話可以消耗更多的脂肪。<br><br><h3>
+2、在車上使用手環</h3>
+<br>
+站在車上時，用手環的動作，有時用力握緊，有時放鬆，反復做，使手腕變細。<br><br><h3>
+3、保齡球運動</h3>
+<br>
+這是當今許多人經常從事的一項活動。雖然你不會出汗，也不管你的技術如何，玩一個小時的話大約可以消耗250卡路里。<br><br><h3>
+4、唱KTV</h3>
+<br>
+比保齡球更加的容易，但要拿著麥克風大聲而用力地唱歌，你至少可以消耗200卡路里！<br><br><h3>
+5、走路時，胸口挺直，小腹緊繃，臀部緊繃</h3>
+<br>
+懶散著走路的話會讓我們的形體很難看，我們要改掉拖著走、駝背走路的習慣。如果你有一個手提包，最好在走路的時候交換一下你的手。<br><br><h3>
+6、熨衣服</h3>
+<br>
+懶人減肥運動中還包括著熨衣服這個家務，平日懶惰的人，從今天起，要朝著“賢慧”的方向前進。雖然它只是熨衣服，但也能在一小時內消耗110-160卡路里！<br><br><h3>
+7、刷牙和洗臉</h3>
+<br>
+早上和晚上，當你洗臉和刷牙時，你可以踮起腳趾，讓腳跟上下移動。<br><br><h3>
+8、掃地+拖地</h3>
+<br>
+家務真的很麻煩，但是如果你把它們看做是一個減肥運動的話，你可能會更加的願意做一點。不要低估這些瑣碎的家務事，40分鐘的拖地可以消耗150卡路里，5分鐘的折疊被子大約25卡路里，20分鐘的使用吸塵器可以消耗47卡路里。<br><br><h3>
+9、換床單</h3>
+<br>
+換床單也可以消耗卡路里，所以以後就不要懶惰了，要勤快的換床單。<br><br><h3>
+10、開始自己做飯菜</h3>
+<br>
+對於有些人來說這是不可能的，但是如果你能做到自己做飯的話，就可以讓自己的在五十分鐘的時間內消耗128的卡路里。<br><br><h3>11、坐一個公車站，還不如步行</h3><br><br>
+懶人減肥運動中還包括了走路這個輕鬆的運動，只要可以步行的話就永遠不要選擇坐公車，有了這樣的認識，每天步行20或30分鐘以上對身心都是絕對有益的。<br>
+以上的這些懶人減肥運動效果都非常的好，非常適合懶人，如果你也屬於懶人的話，你們在減肥的時候不妨試試這些方法。<br></div></div></div><div class="wb_cont_outer"><div id="wb_element_instance12" class="wb_element wb_element_shape" data-plugin="Shape"><a class="wb_shp"></a></div></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 117px;"><div id="wb_element_instance9" class="wb_element wb_text_element" data-plugin="TextArea" style=" line-height: normal;"><p class="wb-stl-footer">© 2020 <a href="https://calfitness.com.hk/">calfitness.com.hk</a></p>
+</div><div id="wb_element_instance13" class="wb_element" data-plugin="" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
